7bf2eea35ae816dd39e3c478b7337682ebd2939c
wenderf5 <wenderfabianoalves@gmail.com>
2026-04-28 16:10:54 -0300

    feat: added User at domain

--- src/main/java/com/financewallet/auth/domain/entities/User.java
+++ src/main/java/com/financewallet/auth/domain/entities/User.java
@@ -1,5 +1,71 @@
 package com.financewallet.auth.domain.entities;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 public class User {
-    
+    private UUID id;
+    private String userName;
+    private String email;
+    private String password;
+    private String photoUrl;
+    private LocalDateTime createdAt;
+
+    public User(UUID id, String userName, String email, String password, String photoUrl, LocalDateTime createdAt) {
+        this.id = id;
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+        this.photoUrl = photoUrl;
+        this.createdAt = createdAt;
+    }
+
+    public User(String userName, String email, String password, String photoUrl) {
+        this.id = UUID.randomUUID();
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+        this.photoUrl = photoUrl;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
 }
