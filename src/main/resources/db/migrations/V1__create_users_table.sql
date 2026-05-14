bd31745257c22af309552faef452ed3064808d5d
wenderf5 <wenderfabianoalves@gmail.com>
2026-05-14 18:43:50 -0300

    migration: created the migration 'V1__create_users_table' that creates the table users in database

---
+++ src/main/resources/db/migrations/V1__create_users_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users (
+    id UUID PRIMARY KEY NOT NULL,
+    user_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    photo_url VARCHAR(500) NOT NULL,
+    created_at TIMESTAMP NOT NULL
+);
