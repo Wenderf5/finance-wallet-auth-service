cf04b74d50d481ad56d477d149b6d7e9b672e052
wenderf5 <wenderfabianoalves@gmail.com>
2026-05-15 22:39:19 -0300

    BEAF

--- src/main/java/com/financewallet/auth/infrastructure/adapters/in/controllers/dto/StartUserCreationRequest.java
+++ src/main/java/com/financewallet/auth/infrastructure/adapters/in/controllers/dto/StartUserCreationRequest.java
@@ -1,4 +1,26 @@
 package com.financewallet.auth.infrastructure.adapters.in.controllers.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class StartUserCreationRequest {
+    @NotBlank(message = "The username cannot be blank")
+    private String userName;
+
+    @NotBlank(message = "The email address cannot be blank")
+    @Email(message = "The email address must be valid")
+    private String email;
+
+    @NotBlank(message = "The password cannot be blank")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?!.*\\s).{8,}$", message = "The password must be at least 8 characters long, contain no spaces, and include at least one uppercase letter, one lowercase letter, and one number")
+    private String password;
 }
