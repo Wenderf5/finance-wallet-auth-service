1c96b1726aaec0e364b7c15122443366d5474c5d
wenderf5 <wenderfabianoalves@gmail.com>
2026-06-18 15:27:15 -0300

    feat: added the controller that receives the token via the Cookie 'signup_session' and their unit tests

--- src/main/java/com/financewallet/auth/infrastructure/adapter/in/controller/auth/AuthController.java
+++ src/main/java/com/financewallet/auth/infrastructure/adapter/in/controller/auth/AuthController.java
@@ -5,12 +5,15 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.financewallet.auth.application.usercase.StartUserRegistrationUseCase;
+import com.financewallet.auth.application.usercase.ValidateSignUpSessionUseCase;
 import com.financewallet.auth.infrastructure.adapter.in.controller.auth.dto.StartUserRegistrationRequest;
 
 import jakarta.validation.Valid;
@@ -19,10 +22,15 @@ import jakarta.validation.Valid;
 @RequestMapping("/api/v1/auth")
 public class AuthController {
     private final StartUserRegistrationUseCase startUserRegistrationUseCase;
+    private final ValidateSignUpSessionUseCase validateSignUpSessionUseCase;
 
     @Autowired
-    public AuthController(StartUserRegistrationUseCase startUserRegistrationUseCase){
+    public AuthController(
+        StartUserRegistrationUseCase startUserRegistrationUseCase,
+        ValidateSignUpSessionUseCase validateSignUpSessionUseCase
+    ){
         this.startUserRegistrationUseCase = startUserRegistrationUseCase;
+        this.validateSignUpSessionUseCase = validateSignUpSessionUseCase;
     }
 
     @PostMapping("/sign-up")
@@ -49,6 +57,15 @@ public class AuthController {
         return ResponseEntity
             .status(HttpStatus.OK)
             .header(HttpHeaders.SET_COOKIE, signUpSessionCookie.toString())
+            .build();
+    }
+
+    @GetMapping("/sign-up/session")
+    public ResponseEntity<Void> validateSignUpSession(@CookieValue(value = "signup_session", required = false) String signUpSessionToken){
+        this.validateSignUpSessionUseCase.execute(signUpSessionToken);
+        
+        return ResponseEntity
+            .status(HttpStatus.OK)
             .build();
     }
 }
