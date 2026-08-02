9528fa6d13d669b1884bd7637b6ef42de82484f2
wenderf5 <wenderfabianoalves@gmail.com>
2026-08-02 14:30:14 -0300

    chore: removing unused stuff

--- src/test/java/com/financewallet/auth/application/usecase/RefreshTokenUseCaseTest.java
+++ src/test/java/com/financewallet/auth/application/usecase/RefreshTokenUseCaseTest.java
@@ -1,6 +1,5 @@
 package com.financewallet.auth.application.usecase;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
